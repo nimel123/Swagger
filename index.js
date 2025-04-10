83d5212cb04055c1d2ea1e96860a1aa05a5d2d6a
sanju50729 <sanju99658@gmail.com>
2025-04-10 12:06:59 +0530

    new server

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const {connectDB}=require('./Connection/Connection');
 const userRoutes=require('./Routes/routes')
 
 const app = express();
-const port = 9000;
+const port = process.env.PORT || 9000;
+
 
 // Middleware
 app.use(express.json());
